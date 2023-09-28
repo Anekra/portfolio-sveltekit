f6ae9e1f9329e37529f6857603380bc0b119e734
Andika Eka Putra <andika.eka.102@gmail.com>
2023-09-29 00:09:11 +0700

    refactor: update main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,25 @@
 <script>
-	import Navbar from '$lib/Navbar.svelte';
+	import CustomCursor from '$lib/components/CustomCursor.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+	import Navbar from '$lib/components/Navbar.svelte';
 	import '../app.css';
+
+	/**
+	 * @type {HTMLDivElement}
+	 */
+	let circle;
 </script>
 
 <svelte:head>
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=true>
-<link href="https://fonts.googleapis.com/css2?family=Andika&family=Baskervville&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Andika&family=Baskervville&display=swap"
+		rel="stylesheet"
+	/>
 </svelte:head>
 
 <Navbar />
+<CustomCursor bind:cursor={circle} />
 <slot />
+<Footer />
