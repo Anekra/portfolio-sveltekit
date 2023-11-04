a9fc808b9d4906d812e2cf7035761e61fade7984
Andika Eka Putra <andika.eka.102@gmail.com>
2023-11-04 15:54:33 +0700

    feature: add intersection observer

---
+++ src/lib/utils/intersectionObserver.js
@@ -0,0 +1,29 @@
+/** @type {IntersectionObserver} */
+let intersectionObserver;
+
+function ensureIntersectionObserver() {
+	if (intersectionObserver) return;
+
+  intersectionObserver = new IntersectionObserver(
+		(entries) => {
+			entries.forEach(entry => {
+				const eventName = entry.isIntersecting ? 'enterViewport' : 'exitViewport';
+				entry.target.dispatchEvent(new CustomEvent(eventName));
+				if (entry.isIntersecting) entry.target.classList.add('show')
+			});
+		}
+	);
+}
+
+/**  @param {Element} element */
+export default function viewport(element) {
+	ensureIntersectionObserver();
+
+	intersectionObserver.observe(element);
+
+	return {
+		destroy() {
+			intersectionObserver.unobserve(element);
+		}
+	}
+}
