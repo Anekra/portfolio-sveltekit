6810a7340df4d22c421fb81babe09b03e89eeae2
Andika Eka Putra <andika.eka.102@gmail.com>
2023-11-04 15:53:03 +0700

    feature: add email validation util function

---
+++ src/lib/utils/emailRegExCheck.js
@@ -0,0 +1,7 @@
+export const emailRegExCheck = (/** @type {string  | FormDataEntryValue} */ email) => {
+	const emailRegEx =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	const isEmailValid = emailRegEx.test(String(email).toLowerCase());
+
+	return isEmailValid;
+};
