9d895d6af2732636bfdfb55be61f46f7b425a5b7
Andika Eka Putra <andika.eka.102@gmail.com>
2023-11-04 15:29:34 +0700

    refactor: make the footer responsive

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,69 +2,48 @@
 	import Icon from '@iconify/svelte';
 	import Logo from '$lib/assets/icons/portfolio_logo_icon.svelte';
 	import { theme as darkMode } from '$lib/stores/theme';
-	import { handleHover } from '$lib/utils/handleHover';
-	import { handleFocus } from '$lib/utils/handleFocus';
+	import { page } from '$app/stores';
 </script>
 
 <footer
-	class="flex items-center gap-2 rounded-[10px_10px_0_0] bg-primary py-6 shadow-[0_-0.5px_0.5px_#334041] dark:bg-primaryContainer dark:shadow-[0_-0.5px_0.5px_#69ABFF]"
+	class="absolute bottom-0 flex w-screen flex-col items-center justify-between py-2 shadow-[0_-0.5px_2px_#334041] dark:shadow-[0_-0.5px_2px_#ffffff] sm:h-[127px]"
 >
-	<div class="flex flex-grow justify-center gap-2">
-		<button
-			class="btn"
-			on:mouseover={handleHover}
-			on:mouseleave={handleHover}
-			on:focus={() => handleFocus}
-		>
-			<Icon icon="mdi:github" class="text-5xl" />
-		</button>
-		<button
-			class="btn"
-			on:mouseover={handleHover}
-			on:mouseleave={handleHover}
-			on:focus={() => handleFocus}
-		>
-			<Icon icon="mdi:google" class="text-5xl" />
-		</button>
-		<button
-			class="btn"
-			on:mouseover={handleHover}
-			on:mouseleave={handleHover}
-			on:focus={() => handleFocus}
-		>
-			<Icon icon="mdi:linkedin" class="text-5xl" />
-		</button>
-		<button
-			class="btn"
-			on:mouseover={handleHover}
-			on:mouseleave={handleHover}
-			on:focus={() => handleFocus}
-		>
-			<Icon icon="mdi:instagram" class="text-5xl" />
-		</button>
+	<div class="flex h-full w-full items-center justify-around py-1">
+		<div class="flex w-36 mobileMd:flex-wrap justify-center gap-2 sm:w-64 md:w-72 px-4">
+			<a href="https://github.com/anekra" class="btn">
+				<Icon icon="mdi:github" class="text-3xl sm:text-4xl md:text-5xl" />
+			</a>
+			<a href="#google" class="btn">
+				<Icon icon="mdi:google" class="text-3xl sm:text-4xl md:text-5xl" />
+			</a>
+			<a href="#linkdin" class="btn">
+				<Icon icon="mdi:linkedin" class="text-3xl sm:text-4xl md:text-5xl" />
+			</a>
+			<a href="#instagram" class="btn">
+				<Icon icon="mdi:instagram" class="text-3xl sm:text-4xl md:text-5xl" />
+			</a>
+		</div>
+		<div class="flex w-36 justify-center sm:w-64 md:w-72">
+			<button
+				class="btn flex cursor-pointer items-center gap-1 border border-black px-2 py-1 hh:hover:border-transparent dark:border-white hh:dark:hover:border-transparent"
+			>
+				<Icon icon="mdi:google-play" class="text-2xl sm:text-4xl" />
+				<div class="flex flex-col items-start gap-1 leading-none">
+					<h3 class="text-[8px] sm:text-[11px]">GET IT ON</h3>
+					<h3 class="text-start text-[12px] font-bold sm:text-lg sm:tracking-widest">Google Play</h3>
+				</div>
+			</button>
+		</div>
 	</div>
-	<div class="flex flex-grow flex-col items-center gap-2">
+	<div class="absolute sm:top-3 mobileMd:top-8 mx-4 flex">
 		<a
-			href="/"
-			class="btn"
-			on:mouseover={handleHover}
-			on:mouseleave={handleHover}
-			on:focus={() => handleFocus}><Logo color={$darkMode ? 'white' : 'black'} /></a
+			href={$page.route.id}
+			class="btn border border-black p-1 hh:hover:border-transparent dark:border-white hh:dark:hover:border-transparent sm:p-2"
 		>
-		<h1 class="text-xl text-onPrimary dark:text-secondary">© 2023 Andika Eka Putra</h1>
+			<Logo color={$darkMode ? 'white' : 'black'} />
+		</a>
 	</div>
-	<div class="flex flex-grow justify-center">
-		<button
-			class="btn flex cursor-pointer items-center gap-1 rounded border border-black px-2 py-1 hover:border-transparent dark:hover:border-transparent dark:border-white"
-			on:mouseover={handleHover}
-			on:mouseleave={handleHover}
-			on:focus={() => handleFocus}
-		>
-			<Icon icon="mdi:google-play" class="text-4xl" />
-			<div class="flex flex-col items-start leading-none">
-				<h3 class="text-[11px]">GET IT ON</h3>
-				<h3 class="text-lg font-bold tracking-widest">Google Play</h3>
-			</div>
-		</button>
+	<div class="flex justify-center">
+		<h1 class="text-xl text-onPrimary dark:text-secondary">© 2023 Andika Eka Putra</h1>
 	</div>
 </footer>
