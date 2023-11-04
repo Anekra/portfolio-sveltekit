61836cef6d68147ac47c2d1c80db806e97fc2390
Andika Eka Putra <andika.eka.102@gmail.com>
2023-11-04 15:41:18 +0700

    feature: add sidebar, remove custom cursor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,20 @@
 <script>
-	import CustomCursor from '$lib/components/CustomCursor.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
+	import Sidebar from '$lib/components/Sidebar.svelte';
 	import '../app.css';
-
-	/**
-	 * @type {HTMLDivElement}
-	 */
-	let circle;
 </script>
 
 <svelte:head>
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
 	<link
-		href="https://fonts.googleapis.com/css2?family=Andika&family=Baskervville&display=swap"
+		href="https://fonts.googleapis.com/css2?family=Andika:wght@400;700&family=Baskervville&display=swap"
 		rel="stylesheet"
 	/>
 </svelte:head>
 
 <Navbar />
-<CustomCursor bind:cursor={circle} />
+<Sidebar />
 <slot />
 <Footer />
